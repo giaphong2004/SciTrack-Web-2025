01e0984efa88835e875e1ad3336dba593ec44c81
giaphong2004 <2200010465@nttu.edu.vn>
2025-10-08 11:51:00 +0700

    feat: Khởi tạo solution SciTrack (Web MVC + Web API); bật swagger,CORS(dev);cấu hình để fe có thể gọi API

---
+++ SciTrack.Api/Controllers/TSKHCNController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SciTrack.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TSKHCNController : Controller
+    {
+        private static readonly List<object> _data = new() {
+        new { id = 1, maSo = "TS001", ten = "Máy in HP", nguyenGia = 5000000m, trangThai="Đang sử dụng" }
+    };
+
+        [HttpGet] public IActionResult Get() => Ok(_data);
+    }
+}
---
+++ SciTrack/Services/TaiSanApi.cs
@@ -0,0 +1,10 @@
+﻿namespace SciTrack.web.Services
+{
+    public class TaiSanApi
+    {
+        private readonly HttpClient _http;
+        public TaiSanApi(IHttpClientFactory f) => _http = f.CreateClient("api");
+        public async Task<List<dynamic>?> GetListAsync() =>
+            await _http.GetFromJsonAsync<List<dynamic>>("/api/v1/tskhcn");
+    }
+}
