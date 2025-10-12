1a775a533d78770bb6e537c87cc21d31fbded2db
DinhTien <ndt251004@gmail.com>
2025-10-12 13:38:45 +0700

    them layout

---
+++ SciTrack/Controllers/TaiSanController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SciTrack.web.Models;
+
+namespace SciTrack.web.Controllers
+{
+    public class TaiSanController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<TaiSan>
+            {
+                new TaiSan { MaTaiSan = "TS001", TenTaiSan = "Máy tính Dell", TrangThai = "Đang sử dụng" },
+                new TaiSan { MaTaiSan = "TS002", TenTaiSan = "Máy in Canon", TrangThai = "Bảo trì" },
+                new TaiSan { MaTaiSan = "TS003", TenTaiSan = "Bộ đo nhiệt độ", TrangThai = "Tốt" }
+            };
+            return View(list);
+        }
+    }
+}
---
+++ SciTrack/Models/TaiSan.cs
@@ -0,0 +1,15 @@
+﻿namespace SciTrack.web.Models
+{
+    public class TaiSan
+    {
+        public string MaTaiSan { get; set; }
+        public string TenTaiSan { get; set; }
+        public string TrangThai { get; set; }
+        public decimal NguyenGia { get; set; }
+        public decimal KhauHao { get; set; }
+        public decimal HaoMon { get; set; }
+        public decimal GiaTriConLai { get; set; }
+        public DateTime NgayCapNhat { get; set; }
+        public string MaSoDeTai { get; set; }
+    }
+}
